812ca2e6073ffe5c7ffde9013268dcce2528b564
fish267 <fsh267@gmail.com>
2017-10-31 16:36:43 +0800

    modify testcase

--- test/testcase/test_cli.py
+++ test/testcase/test_cli.py
@@ -1,12 +1,12 @@
 import sys
 import unittest
 
-from six_web import __version__
-from six_web.cli import _main
+from six_web.cli import _cli_parse
 
 
 class TestSixConfig(unittest.TestCase):
     def test_add(self):
         test_args = sys.argv
         test_args.append('--version')
-        self.assertEqual(__version__, _main(test_args))
+        test_args.append('-bind 127.0.0.1:8888')
+        _cli_parse(test_args)
